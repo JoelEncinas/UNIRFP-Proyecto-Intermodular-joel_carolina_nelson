0a41b359fb370eefe6ae0ee42c883e0a6a532e5e
jencinas <joelencinasmartin@gmail.com>
2026-03-05 23:49:28 +0100

    crear estructura global de angular

---
+++ frontend/src/app/features/map/pages/map-page/map-page.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-map-page',
+  imports: [],
+  templateUrl: './map-page.html',
+  styleUrl: './map-page.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MapPage {}
---
+++ frontend/src/app/features/terms/pages/terms-page/terms-page.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-terms-page',
+  imports: [],
+  templateUrl: './terms-page.html',
+  styleUrl: './terms-page.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TermsPage {}
