153831312fc290fd837d29315c29ecb530869d18
jencinas <joelencinasmartin@gmail.com>
2026-03-16 08:18:22 +0100

    añadida interfaz adaptada al backend en el frontend para autentificacion

--- frontend/src/app/features/auth/models/auth.models.ts
+++ frontend/src/app/features/auth/models/auth.models.ts
@@ -1,1 +1,14 @@
-export interface Auth {}
+export interface LoginRequest {
+  username: string;
+  password: string;
+}
+
+export interface RegisterRequest {
+  username: string;
+  email: string;
+  password: string;
+}
+
+export interface AuthResponse {
+  token: string;
+}
